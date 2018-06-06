f48afd077122b2f3ea19a5a39c4d413494d7e44a
SHIVANG CHOPRA <shivangchopra11@gmail.com>
2018-06-06 18:02:40 +0530

    Basic app done

---
+++ Assets/AnchorMap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class AnchorMap : MonoBehaviour {
+
+	// Use this for initialization
+	// void Start () {
+		
+	// }
+	
+	// Update is called once per frame
+	void Update () {
+		Touch touch = Input.GetTouch(0);
+
+		if(Input.touchCount<1 || touch.phase != TouchPhase.Began) {
+			return;
+		}
+
+		// raycast against the point where the player touched
+		TrackableHit hit;
+
+		TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinBounds|TrackableHitFlags.PlaneWithinPolygon|TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+		if(Frame.Raycast(touch.position.x,touch.position.y,raycastFilter,out hit)) {
+			var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+			transform.position = anchor.transform.position;
+		}
+
+		
+	}
+}
